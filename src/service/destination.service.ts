3bdaf530f44c10fbcda77e3116711ca0271b4aba
Godji Berry <verginia.razvigorova@gmail.com>
2023-09-22 08:41:27 +0300

    Updated destination.service

--- src/service/destination.service.ts
+++ src/service/destination.service.ts
@@ -1,12 +1,55 @@
+// essential part of your application's architecture. It separates the business logic of your application from the controllers and routes,
+//promoting modularity and maintainability. Each of your route handlers (createDestinationHandler, getDestinationsHandler, etc.) should delegate
+//the actual business logic to the corresponding service function.
+//In your service layer, you can encapsulate operations related to your data mode.
+
 import { DestinationI, DestinationM } from "../models/destination.model";
 
-export async function createDestination (input: DestinationI) {
-    try {
-        return await DestinationM.create(input)
-    } catch(e:any) {
-        throw new Error(e)
-    }
+export async function createDestination(input: DestinationI) {
+  try {
+    return await DestinationM.create(input);
+  } catch (e: any) {
+    throw new Error(e);
+  }
 }
 
+export async function getDestinations() {
+  try {
+    return await DestinationM.find().exec();
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
 
-// I don't need to add deleteDestination, getDestinations, getDestinationById services
+export async function getDestinationById(id: string) {
+  try {
+    return await DestinationM.findById(id);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function updateDestination(
+  id: string,
+  updates: Partial<DestinationI>
+) {
+  try {
+    const updatedDestination = await DestinationM.findByIdAndUpdate(
+      id,
+      updates,
+      { new: true } // Return the updated document
+    );
+
+    return updatedDestination;
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function deleteDestination(id: string) {
+  try {
+    return await DestinationM.findByIdAndDelete(id);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
